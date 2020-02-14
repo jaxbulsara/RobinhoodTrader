d38ba1a1469e2edbfaee7edc45ff7d10878b9902
jaxbulsara <jaxbulsara@gmail.com>
2020-02-14 16:14:13 -0500

    Successful test of login with QR code.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,19 @@
 import pytest
 from Robinhood import Robinhood
+from Robinhood.config import getConfiguration
 
 
-def test_login():
-    pass
+def test_login_qrCode(maintainConfig):
+    config = getConfiguration()
+    qrCode = input("QR Code: ")
+    config.set("login", "qrCode", qrCode)
+    with open("config.ini", "w") as configFile:
+        config.write(configFile)
+
+    trader = Robinhood()
+    trader.login()
+
+    assert trader.session.siteAuthToken != None
+    assert trader.session.refreshToken != None
+    assert trader.session.headers["Authorization"].startswith("Bearer: ")
+
