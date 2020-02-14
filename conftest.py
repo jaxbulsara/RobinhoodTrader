9d6dd8011ec534be9b6787404b669b56d24f2762
jaxbulsara <jaxbulsara@gmail.com>
2020-02-14 15:37:43 -0500

    Moved maintainConfig pytest fixture to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,2 +1,11 @@
 import pytest
 
+
+@pytest.fixture
+def maintainConfig():
+    os.chdir("Robinhood/")
+    shutil.copy2("config.ini", "config.ini.bak")
+    yield None
+    os.remove("config.ini")
+    os.rename("config.ini.bak", "config.ini")
+    os.chdir("../")
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,16 +4,6 @@ import os
 from Robinhood.config import getConfiguration, getQrCode
 
 
-@pytest.fixture
-def maintainConfig():
-    os.chdir("Robinhood/")
-    shutil.copy2("config.ini", "config.ini.bak")
-    yield None
-    os.remove("config.ini")
-    os.rename("config.ini.bak", "config.ini")
-    os.chdir("../")
-
-
 def test_getConfiguration(maintainConfig):
     config = getConfiguration()
 
