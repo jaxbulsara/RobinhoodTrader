616ae241dc8ec6491fea2ea6faa107f578b42bce
jaxbulsara <jaxbulsara@gmail.com>
2020-02-15 00:05:42 -0500

    Deleted old files.

--- login.py
+++
@@ -1,9 +0,0 @@
-# logs into robinhood
-
-from Robinhood.Robinhood.Robinhood import Robinhood
-
-QR = "4SMM5FRDEASR7W6Y"
-trader = Robinhood()
-login = trader.login(username="jaxbulsara@gmail.com", password="Relatr4bivity", qr_code=QR)
-quote_info = trader.quote_data("BTC")
-print(quote_info)
