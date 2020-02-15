1ccaeb4dab52f3489305d68dc98999814bea6d90
Jay Bulsara <jaxbulsara@gmail.com>
2020-02-15 18:19:11 -0500

    Added test for the Broker class.

---
+++ tests/test_broker.py
@@ -0,0 +1,7 @@
+from RobinhoodTrader import RobinhoodTrader
+
+def test_investmentProfile(robinhoodTrader: RobinhoodTrader):
+    investmentProfile = robinhoodTrader.getInvestmentProfile()
+
+    assert type(investmentProfile).__name__ == "dict"
+    assert "user" in investmentProfile.keys()
