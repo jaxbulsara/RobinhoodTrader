5f2742cacc9f20e919f3ca781eb13db091abcd27
Jay Bulsara <jaxbulsara@gmail.com>
2020-02-15 13:37:04 -0500

    Updated test_login to test Cryto and Stock Broker session status. All tests passing.

--- tests/test_login.py
+++ tests/test_login.py
@@ -20,13 +20,15 @@ def test_login_logout(maintainConfig):
     assert trader.session.refreshToken != None
     assert trader.session.headers["Authorization"].startswith("Bearer: ")
     assert trader.session.isLoggedIn == True
-    assert trader.broker.session == trader.session
+    assert trader.stockBroker.session == trader.session
+    assert trader.cryptoBroker.session == trader.session
 
     trader.logout()
     
     assert trader.session.siteAuthToken == None
     assert trader.session.headers["Authorization"] == None
     assert trader.session.isLoggedIn == False
-    assert trader.broker.session == None
+    assert trader.stockBroker.session == None
+    assert trader.cryptoBroker.session == None
 
 
