8e6d60b94933a1c6ca707f11f70f355528fb82b0
Jay Bulsara <jaxbulsara@gmail.com>
2020-02-15 14:57:12 -0500

    Created robinhoodTrader fixture. moved login and logout tests to the fixture.

--- RobinhoodTrader/broker/Broker.py
+++ RobinhoodTrader/broker/Broker.py
@@ -1,15 +1,20 @@
 from RobinhoodTrader import endpoints
-import warnings, requests
+from RobinhoodTrader.session import RobinhoodSession
+import warnings, requests, pprint
 
 
 class Broker:
     def __init__(self):
         pass
 
-    def getInvestmentProfile(self, session):
+    def getInvestmentProfile(self, session: RobinhoodSession):
         try:
             response = session.get(endpoints.investmentProfile(), timeout=15)
             response.raise_for_status()
+            responseData = response.json()
+            pprint.pprint(responseData, indent=4)
+            return responseData
+            
         except requests.exceptions.HTTPError:
             warnings.warn(
                 "You must log in before you can access your investment profile."
--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 import pytest
 import shutil
 import os
+from RobinhoodTrader import RobinhoodTrader
+from RobinhoodTrader.config import getConfiguration
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def maintainConfig():
     os.chdir("RobinhoodTrader/")
     shutil.copy2("config.ini", "config.ini.bak")
@@ -11,3 +13,26 @@ def maintainConfig():
     os.remove("config.ini")
     os.rename("config.ini.bak", "config.ini")
     os.chdir("../")
+
+
+@pytest.fixture(scope="session")
+def robinhoodTrader(maintainConfig):
+    config = getConfiguration()
+    qrCode = input(
+        "QR Code (Leave blank to use SMS or Authenticator app code): "
+    )
+    if qrCode == "":
+        qrCode = "None"
+    config.set("login", "qrCode", qrCode)
+    with open("config.ini", "w") as configFile:
+        config.write(configFile)
+
+    trader = RobinhoodTrader()
+
+    yield trader
+
+    trader.logout()
+
+    assert trader.session.siteAuthToken == None
+    assert trader.session.headers["Authorization"] == None
+    assert trader.session.isLoggedIn == False
