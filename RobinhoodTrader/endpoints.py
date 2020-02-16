e2ca207ad1ab6a45a0d5c69538c06a4b0d631a26
jaxbulsara <jaxbulsara@gmail.com>
2020-02-15 22:29:35 -0500

    Add some endpoints.

--- RobinhoodTrader/endpoints.py
+++ RobinhoodTrader/endpoints.py
@@ -1,4 +1,5 @@
-api = "https://api.robinhood.com"
+api = "https://api.robinhood.com/"
+nummus = "https://nummus.robinhood.com/"
 
 
 def login():
@@ -9,5 +10,37 @@ def logout():
     return api + "/oauth2/revoke_token/"
 
 
-def investmentProfile():
-    return api + "/user/investment_profile/"
+def accounts():
+    return api + "accounts/"
+
+
+def user():
+    return api + "user/"
+
+
+def userAdditionalInfo():
+    return api + "user/additional_info/"
+
+
+def userBasicInfo():
+    return api + "user/basic_info/"
+
+
+def userCipQuestions():
+    return api + "user/cip_questions/"
+
+
+def userEmployment():
+    return api + "user/employment/"
+
+
+def userID():
+    return api + "user/id/"
+
+
+def userIdentityMismatch():
+    return api + "user/identity_mismatch/"
+
+
+def userInvestmentProfile():
+    return api + "user/investment_profile/"
