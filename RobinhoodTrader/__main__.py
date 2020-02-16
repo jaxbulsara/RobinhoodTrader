b50cb4d2ef7620042ddc5f889f846addd8d2de0a
jaxbulsara <jaxbulsara@gmail.com>
2020-02-16 15:37:44 -0500

    Update __main__.py

--- RobinhoodTrader/__main__.py
+++ RobinhoodTrader/__main__.py
@@ -1,4 +1,19 @@
-from . import RobinhoodTrader
+from .RobinhoodTrader import RobinhoodTrader
+import pprint, os, shutil
 
-trader = RobinhoodTrader()
-trader.login()
+printer = pprint.PrettyPrinter(indent=4)
+os.chdir("RobinhoodTrader/")
+shutil.copy2("config.ini", "config.ini.bak")
+if os.path.isfile("myConfig.ini"):
+    os.remove("config.ini")
+    shutil.copy2("myConfig.ini", "config.ini")
+
+try:
+    trader = RobinhoodTrader()
+    output = trader.getInvestmentProfile()
+    printer.pprint(output)
+
+except Exception:
+    os.remove("config.ini")
+    os.rename("config.ini.bak", "config.ini")
+    os.chdir("../")
