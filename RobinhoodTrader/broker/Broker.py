05848ad8a338fc3bd85b7cf37ccb6e83544837cc
jaxbulsara <jaxbulsara@gmail.com>
2020-02-16 15:36:10 -0500

    Added authRequired.

--- RobinhoodTrader/broker/Broker.py
+++ RobinhoodTrader/broker/Broker.py
@@ -1,8 +1,10 @@
 from RobinhoodTrader import endpoints
 from RobinhoodTrader.session import RobinhoodSession
+from RobinhoodTrader.session.wrappers import authRequired
 
 
 class Broker:
+    @authRequired
     def getInvestmentProfile(self, session: RobinhoodSession):
         response = session.get(endpoints.userInvestmentProfile(), timeout=15,)
         response.raise_for_status()
