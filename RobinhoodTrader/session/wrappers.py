c4cc110aed62892b99c6ea99dff52acbdbe5690c
jaxbulsara <jaxbulsara@gmail.com>
2020-02-16 15:37:23 -0500

    Fixed some bugs in authRequired wrapper.

--- RobinhoodTrader/session/wrappers.py
+++ RobinhoodTrader/session/wrappers.py
@@ -1,15 +1,21 @@
 import requests
+from RobinhoodTrader import exceptions
 
 
 def authRequired(function):  # pylint: disable=E0213
-    def makeUserLogin(self, session=None, *args, **kwargs):
-        if session == None:
+    def makeUserLogin(self, *args, **kwargs):
+        session = list(
+            filter(lambda arg: type(arg).__name__ == "RobinhoodSession", args)
+        )
+        if session:
+            session = session[0]
+        else:
             session = self
-        if not self.isLoggedIn:
-            self.login(session.credentials)
+        if not session.isLoggedIn:
+            session.login(session.credentials)
         try:
             return function(self, *args, **kwargs)  # pylint: disable=E1102
         except requests.exceptions.HTTPError:
-            print(f"You must login to use this method ({function.__name__}).")
+            raise exceptions.CredentialError
 
     return makeUserLogin
